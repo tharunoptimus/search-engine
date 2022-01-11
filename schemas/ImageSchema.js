704bf8607d6bfdbbba9afde55aff402cf9936f3b
tharunoptimus <tharunoptimus@outlook.com>
2022-01-11 16:39:43 +0530

    Added `siteUrl` field to Image Schema

--- schemas/ImageSchema.js
+++ schemas/ImageSchema.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ImageSchema = new Schema({
+	siteUrl: { type: String, trim: true },
 	url: { type: String, required: true, trim: true },
 	alt: { type: String, trim: true },
-    
 });
 ImageSchema.index({url: 'text', alt: 'text'});
 
